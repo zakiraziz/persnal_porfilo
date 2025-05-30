aa2c83c363636ef0b6089939f32cca5a374158ab
Zakir Aziz <yourGitHubID+zakiraziz@users.noreply.github.com>
2025-05-30 22:52:55 +0500

    my frontend developing jerny

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,52 @@
+import { useEffect } from "react"
+
+export const  Navbar = ({menuOpen, setMenuOpen}) => {
+    useEffect(() => {
+        document.body.style.overflow = menuOpen ? "hidden" : "";
+    })
+    return <nav className="fixed top-0 w-full z-40 bg-[rgba(10, 10, 10, 0.8)] backdrop-blur-lg border-b border-white/10 shadow-lg">
+        <div className="max-w-5xl mx-auto px-4">
+            <div className="flex justify-between items-center h-16">
+                <a href="#home" className="font-mono text-xl font-bold text-white">
+                    {""}
+                    pedro<span className="text-blue-500">.tech</span>{" "}
+                </a>
+
+                <div className="w-7 h-5 relative cursor-pointer z-40 md:hidden">
+                    &#9776;
+                </div>
+
+                <div className="hidden md:flex items-center space-x-8">
+                    <a
+                     href="#home"
+                     className="text-gray-300 hove:text-whitetransition-colors"
+                     > 
+                      {" "}
+                      Home{" "}
+                      </a> 
+                      <a
+                     href="#about"
+                     className="text-gray-300 hove:text-whitetransition-colors"
+                     > 
+                      {" "}
+                      About{" "}
+                      </a> 
+                      <a
+                     href="#projects"
+                     className="text-gray-300 hove:text-whitetransition-colors"
+                     > 
+                      {" "}
+                      Projects{" "}
+                      </a> 
+                      <a
+                     href="#contact"
+                     className="text-gray-300 hove:text-whitetransition-colors"
+                     > 
+                      {" "}
+                      Contact{" "}
+                      </a> 
+                </div>
+            </div>
+        </div>
+    </nav>
+}
